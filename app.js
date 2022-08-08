656ef172048b53a538c0d66594e9b7ca2e0525cf
Hyun Kong <konghyunsik7@gmail.com>
2022-08-08 14:12:25 -0400

    completed

---
+++ app.js
@@ -0,0 +1,32 @@
+//Exericise #1 & 2
+
+const firstName = "Stan";
+let lastName = "Kong";
+var age = 99;
+
+//Exercise #3
+
+let language = "JavaScript";
+let createdYear = 1995;
+var isCaseSensitive = true;
+
+//Exercise #4
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let sectedSize = "M"
+
+//Exericse #5
+
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount= 722;
+let bookmarkt= 456;
+let hasRead = true;
+
+
+
+
